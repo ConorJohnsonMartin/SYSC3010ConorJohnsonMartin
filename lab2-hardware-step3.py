7e58f9d00d62ce56d288e2fd60a00647e6d272ee
Conor Johnson Martin <71397298+ConorJohnsonMartin@users.noreply.github.com>
2020-09-26 20:24:03 -0400

    Add files via upload

---
+++ lab2-hardware-step3.py
@@ -0,0 +1,16 @@
+#import random
+from sense_emu import SenseHat
+
+s = SenseHat()
+
+print('Press right arrow for temperature to be displayed')
+temp = 0
+while True:
+    events = s.stick.get_events()
+    for event in events:
+        if event.action == 'pressed':
+            if event.direction == 'right':  
+                print ('The temperature is', temp)
+                temp += 1
+    
+
