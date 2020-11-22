15d5f5bb24ccf699c3abe611ded44bddbefeb61e
Conor Johnson Martin <71397298+ConorJohnsonMartin@users.noreply.github.com>
2020-11-21 21:43:58 -0500

    Update Moisture_Sensor.py

--- Moisture_Sensor.py
+++ Moisture_Sensor.py
@@ -2,18 +2,18 @@
 import RPi.GPIO as GPIO
 import time
 
-channel = 21
+GPIOchannel = 4
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(GPIOchannel, GPIO.IN)
 
-def callback(channel):
-    if GPIO.input(channel):
+def getMoisture(GPIOchannel):
+    if GPIO.input(GPIOchannel):
         print("Low Water Level")
     else:
         print("High Water Level")
         
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-GPIO.add_event_callback(channel, callback)
+GPIO.add_event_detect(GPIOchannel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(GPIOchannel, getMoisture)
 
 while True:
     time.sleep(1)
